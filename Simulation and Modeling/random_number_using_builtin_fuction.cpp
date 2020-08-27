2d98a6a05592ce3de0151e03035cf863892becc4
Nadim-Mahmud <mahmudnadim16@gmail.com>
2020-08-27 10:37:13 +0600

    built in random number generation completed

---
+++ Simulation and Modeling/random_number_using_builtin_fuction.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x,a,c,m,i,n;
+
+    freopen("random_number_using_builtin_fuction.txt","w",stdout);
+
+    cin>>n>>m;
+
+    for(i=0;i<=n;i++){
+        x = rand()%m;
+        cout<<((double)x/(double)m)<<" ";
+    }
+
+    return 0;
+}
