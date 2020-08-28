5fb827e1ab16a96508a763bc7bcb554f7ff2e86f
Nadim-Mahmud <mahmudnadim16@gmail.com>
2020-08-28 20:42:19 +0600

    chemical reaction done

--- Simulation and Modeling/chemical_reactor.cpp
+++ Simulation and Modeling/chemical_reactor.cpp
@@ -1,28 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define MX 100000
+
+double a[MX],b[MX],c[MX];
+
 int main(){
-    double a,b,c=0,a1,b1,c1,dt,time,i=0,k1,k2;
+    double dt,time,i=0,k1,k2;
+    int in=0;
 
     freopen("chemical_reactor.txt","r",stdin);
 
     cin>>k1>>k2; // rate of reaction
-    cin>>a>>b;   // initial amount of a and b
+    cin>>a[0]>>b[0];   // initial amount of a and b
     time = 5;
     dt = 0.10;
 
-    puts("..........................................");
-    puts("|  Time  |   A(t)   |   B(t)  |   C(t)   |");
-    puts("..........................................");
-    printf("|  %.2lf  |  %.2lf  |  %.2lf  | %2.2lf  |\n",i,a,b,c);
+    puts("........................................");
+    puts("|  Time   |  A(t)   |   B(t)  |  C(t)  |");
+    puts("........................................");
+    printf("|  %5.2lf  |  %5.2lf |  %5.2lf  | %5.2lf  |\n",i,a[0],b[0],c[0]);
 
     for(i=dt;i<=time;i+=dt){
-        a1 = a + (k2*c - k1*a*b)*dt;
-        b1 = b + (k2*c - k1*a*b)*dt;
-        c1 = c + (2*k1*a*b - 2*k2*c)*dt;
-        a = a1,b=b1,c=c1;
-        printf("|  %.2lf  |  %.2lf  |  %.2lf  | %2.2lf  |\n",i,a,b,c);
+        in++;
+        a[in] = a[in-1] + (k2*c[in-1] - k1*a[in-1]*b[in-1])*dt;
+        b[in] = b[in-1] + (k2*c[in-1] - k1*a[in-1]*b[in-1])*dt;
+        c[in] = c[in-1] + (2*k1*a[in-1]*b[in-1] - 2*k2*c[in-1])*dt;
+        printf("|  %5.2lf  |  %5.2lf  |  %5.2lf  | %5.2lf  |\n",i,a[in],b[in],c[in]);
     }
+    puts("........................................");
+
+
+    ///Graphics part :
+
+
+
+
 
     return 0;
 }
+
