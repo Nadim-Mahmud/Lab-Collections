7b8a30bb89f9f1fb9e968e9d91f959c1a86cac74
Nadim Mahmud <mahmudnadim16@gmail.com>
2021-10-09 09:39:27 +0600

    Update basic.cpp
    basic pthread program

--- Parallal processing/PThread/basic.cpp
+++ Parallal processing/PThread/basic.cpp
@@ -1,36 +1,34 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <pthread.h> 
-  
-// Let us create a global variable to change it in threads 
-int g = 0; 
-  
-// The function to be executed by all threads 
-void *myThreadFun(void *vargp) 
-{ 
-    // Store the value argument passed to this thread 
-    int *myid = (int *)vargp; 
-  
-    // Let us create a static variable to observe its changes 
-    static int s = 0; 
-  
-    // Change static and global variables 
-    ++s; ++g; 
-  
-    // Print the argument, static and global variables 
-    printf("Thread ID: %d, Static: %d, Global: %d\n", *myid, ++s, ++g); 
-} 
-  
-int main() 
-{ 
-    int i; 
-    pthread_t tid; 
-  
-    // Let us create three threads 
-    for (i = 0; i < 3; i++) 
-        pthread_create(&tid, NULL, myThreadFun, (void *)&tid); 
-  
-    pthread_exit(NULL); 
-    return 0; 
-} 
+#include<bits/stdc++.h>
+#include<pthread.h>
+
+using namespace std;
+
+int thread_count;
+
+void * hello(void * rank){
+    long my_rank = (long) rank;
+    cout<<"printing from :"<<my_rank<<endl;
+    return NULL;
+}
+
+int main(int argc, char * argv[]){
+    long thread;
+    pthread_t * thread_handels;
+
+    thread_count = strtol(argv[1], NULL, 10);
+    thread_handels = (pthread_t *) malloc(thread_count * sizeof(pthread_t));
+
+    for(thread = 0; thread < thread_count; thread++){
+        pthread_create(&thread_handels[thread], NULL, hello, (void *) thread);
+    }
+
+    puts("printing from main program");
+
+    for(thread = 0; thread < thread_count; thread++){
+        pthread_join(thread_handels[thread], NULL);
+    }
+
+    free(thread_handels);
+
+    return 0;
+}
