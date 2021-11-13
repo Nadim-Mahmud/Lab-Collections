6b8b2ddd094a45d08e49d2cb84f75171391865e6
Nadim Mahmud <mahmudnadim16@gmail.com>
2021-11-13 14:14:08 +0600

    Update pi_calculaton_for_private_declaration.cpp
    error corrected

--- Parallal processing/OpenMP/pi_calculaton_for_private_declaration.cpp
+++ Parallal processing/OpenMP/pi_calculaton_for_private_declaration.cpp
@@ -17,12 +17,11 @@ int main(int argc, char * argv[]){
     int thread_count = strtol(argv[1],NULL,10);
 
     sum = 0.0;
-    n = 1000;
+    n = 1000000;
 
     // this pragma is a pre processor which will start openmp code if we don't put this , then the programm will run as a seial code of cpp
-    # pragma omp parallel num_threads(thread_count) \
+    # pragma omp parallel for num_threads(thread_count) \
         reduction(+ : sum) private(factor)
-        {
             for(i = 0 ; i <= n ; i++){
                 if(i % 2 == 0){
                     factor = 1.0;
@@ -32,7 +31,6 @@ int main(int argc, char * argv[]){
                 }
                 sum += factor/(2.0 * (double)i + 1);
             }
-        }
 
   
 
